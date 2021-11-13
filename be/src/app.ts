dc1fc3c6460e3ed6a2173c80c10ed2a24b3c1349
Borislav <borislav1207@gmail.com>
2021-11-13 18:11:46 +0200

    update dynamic views

--- be/src/app.ts
+++ be/src/app.ts
@@ -12,21 +12,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/css", express.static(path.join(ROOT_DIR, "/public/css")));
 app.use("/images", express.static(path.join(ROOT_DIR, "/public/images")));
 app.use('/js', express.static(path.join(ROOT_DIR, "/public/js")));
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500);
-//   res.render('404', { error: err });
-// });
 
 app.get("/", (req, res) => {
-  res.render("main");
+  res.render("pages/home");
 });
 
 app.get('/product', (req, res) => {
-  res.render('product');
+  res.render('pages/product');
 });
 
 app.get('/review', (req, res) => {
-  res.render('review');
+  res.render('pages/review');
 });
 
 app.get("/sign-in", (req, res) => {
@@ -57,6 +53,19 @@ app.post("/sign-up", (req, res) => {
   // }
   console.log(email, password);
   res.redirect("/");
+  }
+});
+
+app.get('/shopping-cart', (req, res) => {
+  res.render('pages/cart');
+})
+
+app.get('/product-category', (req, res) => {
+  res.render('pages/product-category');
+})
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('pages/404', { error: err });
 });
 
 app.listen(port, () => {
