1321f0efe3e85134bef0045e4a78081c30a7fc7e
Borislav <borislav1207@gmail.com>
2021-12-19 11:25:22 +0200

    small updates

--- be/src/app.ts
+++ be/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import ejs from "ejs";
 import multer from "multer";
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "public/uploads" });
 import { Pool, Client } from "pg";
 import { fileURLToPath } from "url";
 
@@ -27,18 +27,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/css", express.static(path.join(ROOT_DIR, "/public/css")));
 app.use("/images", express.static(path.join(ROOT_DIR, "/public/images")));
 app.use("/js", express.static(path.join(ROOT_DIR, "/public/js")));
+app.use("/uploads", express.static(path.join(ROOT_DIR, "/public/uploads")));
 
 app.get("/", (req, res) => {
   pool
-    .query(`SELECT * FROM PRODUCTS`)
+    .query(`SELECT * FROM products`)
     .then((results) => res.render("pages/home", { products: results.rows }))
     .catch((err) => {
       throw err;
     });
 });
 
-app.get("/product", (req, res) => {
-  res.render("pages/product");
+app.get("/product/:productId", (req, res) => {
+  const id = req.params.productId;
+  pool
+    .query(`SELECT * FROM products WHERE id=${id};`)
+    .then((results) =>
+      res.render("pages/product", {
+        name: results.rows[0].name,
+        description: results.rows[0].description,
+        price: results.rows[0].price,
+        rating: results.rows[0].rating,
+        images: results.rows[0].images,
+      })
+    )
+    .catch((err) => {
+      throw err;
+    });
 });
 
 app.get("/review", (req, res) => {
@@ -112,30 +127,38 @@ app.get("/create-product", (req, res) => {
     showAlert: false,
   });
 });
-app.post("/create-product", upload.array("images", 3), (req, res) => {
-  const { name, description, price } = req.body;
-  const photos = req.files as any[];
-  const filenames = photos
-    .filter((photo) => photo.fieldname === "images")
-    .map((photo) => photo.filename);
-  console.log(name, description, price, photos);
-  if (!name || !description || !price || !photos) {
-    res.render("pages/create-product", {
-      name: name || "",
-      description: description || "",
-      price: price || "",
-      showAlert: true,
-    });
+app.post(
+  "/create-product",
+  upload.fields([
+    { name: "main-image", maxCount: 1 },
+    { name: "secondary-images", maxCount: 5 },
+  ]),
+
+  (req, res) => {
+    const { name, description, price } = req.body;
+    const photos = req.files as any[];
+    const mainImage = photos["main-image"].map((photo) => photo.filename);
+    const secondaryImages = photos["secondary-images"].map(
+      (photo) => photo.filename
+    );
+    console.log({ mainImage, secondaryImages });
+    if (!name || !description || !price || !photos) {
+      res.render("pages/create-product", {
+        name: name || "",
+        description: description || "",
+        price: price || "",
+        showAlert: true,
+      });
+    } else {
+      pool
+        .query(
+          `INSERT INTO products(name, description, price, images) VALUES('${name}', '${description}', ${price}, '{${mainImage}}');`
+        )
+        .then((results) => res.redirect("/"))
+        .catch((err) => console.error(err));
+    }
   }
-  // else {
-  //   pool
-  //     .query(
-  //       `INSERT INTO products(name, description, price, images) VALUES('${name}', '${description}', ${price}, '{${photo_paths}}');`
-  //     )
-  //     .then((results) => res.redirect("/"))
-  //     .catch((err) => console.error(err));
-  // }
-});
+);
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render("pages/404", { error: err });
